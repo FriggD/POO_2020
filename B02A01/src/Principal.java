0a5009fb99d957dd1910b5694e25ff68a4298399
Glaucia Dias <glaucia.belo.dias@gmail.com>
2020-09-15 14:26:45 -0300

    balanco X

--- B02A01/src/Principal.java
+++ B02A01/src/Principal.java
@@ -2,12 +2,32 @@ public class Principal {
     public static void main(String args[]){
         Agencia novaAgencia= new Agencia(1, 1000.0, 0.005);
 
-        ContaCorrente contaLuiz = novaAgencia.novaContaCorrente("Luiz Pedro Petroski");
-        System.out.println(contaLuiz);
-        ContaCorrente contaMaria = novaAgencia.novaContaCorrente("Maria da Silva");
-        System.out.println(contaMaria);
+        System.out.println("~========+ CONTAS: +========~");
 
-        novaAgencia.cobrarTaxa(contaLuiz);
+        System.out.println("~========+ Correntes +========~");
+        ContaCorrente contaFulano = novaAgencia.novaContaCorrente("Fulano Silva");
+        System.out.println(contaFulano);
+        
+        ContaCorrente contaBeltrano = novaAgencia.novaContaCorrente("Beltrano Silva");
+        System.out.println(contaBeltrano);
 
+        contaFulano.depositar(100.0);
+        contaBeltrano.depositar(50.0);
+
+        novaAgencia.cobrarTaxa(contaFulano);
+
+        System.out.println("~========+ Poupancas +========~");
+        ContaPoupanca contaCiclano = novaAgencia.novaContaPoupanca("Ciclano Souza");
+        System.out.println(contaCiclano);
+        
+        ContaPoupanca contaZeh = novaAgencia.novaContaPoupanca("Zeh");
+        System.out.println(contaZeh);
+    
+        contaCiclano.depositar(400.0);
+        contaZeh.depositar(600.0);
+        
+        novaAgencia.cobrarTaxa(contaZeh);
+
+        novaAgencia.balanco();
     }
 }
